b0b9fb5f0b990fe3827949491cd66fc5ef2ee146
keisei_1092 <keisei_1092@yahoo.co.jp>
2016-12-24 23:31:52 +0900

    Initial Commit

---
+++ TimerTweetViewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TimerTweetViewer
+//
+//  Created by Keisei Saito on 2016/12/24.
+//  Copyright © 2016 keisei_1092. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
