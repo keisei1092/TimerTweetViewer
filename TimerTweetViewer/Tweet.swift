d6f849376f731e123f6749625739de015958de63
keisei_1092 <keisei_1092@yahoo.co.jp>
2016-12-25 01:07:11 +0900

    create tweet class skeleton

---
+++ TimerTweetViewer/Tweet.swift
@@ -0,0 +1,13 @@
+//
+//  Tweet.swift
+//  TimerTweetViewer
+//
+//  Created by Keisei Saito on 2016/12/25.
+//  Copyright © 2016 keisei_1092. All rights reserved.
+//
+
+import UIKit
+
+class Tweet: NSObject {
+
+}
